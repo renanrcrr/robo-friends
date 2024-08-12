c3cf589f9064b080ce162f4f34e0f0fab0be427b
renanrcrr <renanrcrr.developer@gmail.com>
2024-08-13 04:41:13 +0930

    Adding fetch and componentDidMount lifecycle.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,47 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
-import { robots } from './robots';
 import SearchBox from './SearchBox';
 import './App.css';
 
 class App extends Component {
+    // Lifecycle : constructor is the first to run
     constructor(){
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    // Lifecycle : componentDiMount is the third to run
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users }));
     }
 
     onSearchChange = (event) => {
         this.setState({ searchfield: event.target.value });
     }
 
+    // Lifecycle : render is the second to run
     render() {
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-        });
+        const filteredRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
 
-        return (
-            <div className='tc'>
-                <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots}/>
-            </div>
-        );
+        if (this.state.robots.length === 0){
+            return <h1>Loading</h1>
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <CardList robots={filteredRobots}/>
+                </div>
+            );
+        }
+
     }
 }
 
