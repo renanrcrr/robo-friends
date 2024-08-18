99a9cc8199d6aba95151e6255223cc5d7d1d376c
renanrcrr <renanrcrr.developer@gmail.com>
2024-08-19 03:41:06 +0930

    Converting classes to hooks.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,52 +1,59 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import './App.css';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 
-class App extends Component {
+function App() {
     // Lifecycle : constructor is the first to run
-    constructor(){
-        super();
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
-    }
+    // constructor(){
+    //     super();
+    //     this.state = {
+    //         robots: [],
+    //         searchfield: ''
+    //     }
+    // }
+
+    const [robots, setRobots] = useState([])
+    const [searchfield, setSearchfield] = useState('')
+    
 
     // Lifecycle : componentDiMount is the third to run
-    componentDidMount() {
+    // componentDidMount() {
+    //     fetch('https://jsonplaceholder.typicode.com/users')
+    //         .then(response => response.json())
+    //         .then(users => this.setState({ robots: users }));
+    // }
+
+    //It works like componentDidMount and componentDidUpdate
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
-            .then(users => this.setState({ robots: users }));
+            .then(users => setRobots(users));
+    })
+
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value);
     }
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value });
-    }
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    })
 
-    // Lifecycle : render is the second to run
-    render() {
-        const { robots, searchfield } = this.state;
-        const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-        })
-
-        return !robots.length ?
-            <h1>Loading</h1> :
-            (
-                <div className='tc'>
-                    <h1 className='f1'>RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
-                    <Scroll>
-                        <ErrorBoundry>
-                            <CardList robots={filteredRobots}/>
-                        </ErrorBoundry>
-                    </Scroll>
-                </div>
-            );
-    }
+    return !robots.length ?
+        <h1>Loading</h1> :
+        (
+            <div className='tc'>
+                <h1 className='f1'>RoboFriends</h1>
+                <SearchBox searchChange={onSearchChange} />
+                <Scroll>
+                    <ErrorBoundry>
+                        <CardList robots={filteredRobots}/>
+                    </ErrorBoundry>
+                </Scroll>
+            </div>
+        );
 }
 
 export default App;
